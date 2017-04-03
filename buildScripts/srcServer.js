c5b3bb6219ac743670b6c5be87dfe0186c7c289e
Your Name <you@example.com>
2017-04-03 14:09:57 +0200

    end of module 10

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,16 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
+
+app.get('/users', function(req, res) {
+    res.json([
+        { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
+        { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tammy@gmail.com" },
+        { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "tina@gmail.com" }
+
+
+    ]);
+});
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
